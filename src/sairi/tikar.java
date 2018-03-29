929bced75fa26c05c389612f795a8ba18bcb2bde
kusairi <riskadelina96@gmail.com>
2018-03-29 17:23:47 +0800

    membuat attribut dan behaviorr

--- src/sairi/tikar.java
+++ src/sairi/tikar.java
@@ -1,4 +1,22 @@
 package sairi;
 public class tikar {
-    
+       double lebar;
+      double panjang;
+
+    public tikar() {
+        panjang=50;
+        lebar=210;
+    }
+
+    public tikar(double lebar, double panjang) {
+        this.lebar = lebar;
+        this.panjang = panjang;
+    }
+      
+      
+      
+      void cetakInfo(){
+          System.out.println("Panjang       :"+lebar);
+          System.out.println("Panjang       :"+panjang); 
+}
 }
