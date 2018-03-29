7f82fe073319c07f7b8c9baf6ff82fa39b6dd704
kusairi <riskadelina96@gmail.com>
2018-03-29 17:32:30 +0800

    mengisi nilai attribut

--- src/sairi/tikaraksi.java
+++ src/sairi/tikaraksi.java
@@ -2,5 +2,7 @@ package sairi;
 public class tikaraksi {
     public static void main(String[] args) {
             tikar r1 = new tikar ();
+            r1.panjang = 10;
+            r1.lebar = 4;
     }
 }
